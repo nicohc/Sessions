94ce8af6640423d363a8ef4350354cbc564fa0f5
Nico <nico1099@hotmail.fr>
2018-02-05 22:58:21 +0100

    Base site log in SSL et Puma

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if  @user.save
+      flash[:success] = "Bienvenue, Vous êtes bien inscrits." #Permet d'afficher une pop-up de confirmation de l'inscription.
       redirect_to @user  #Si la création du nouveau profil est bien réalisée, alors on fait afficher le profil crée via la fonction SHOW
     else
       render 'new'
