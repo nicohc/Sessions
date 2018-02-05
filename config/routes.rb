bb0365933df9d878647060c639341131af1018fb
Nico <nico1099@hotmail.fr>
2018-02-05 23:51:04 +0100

    Base site log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'users#home'
   resources :users
+
+  get '/login',      to: "sessions#new"
+  post '/login',     to: "sessions#create"
+  delete '/logout',  to: "sessions#destroy"
+
 
 
   resources :moussaillons
